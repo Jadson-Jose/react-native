61ae54790c54b30545ffca7d1ed4712e1341d0b4
JadsonJose <jadsonjosesilva@gmail.com>
2020-08-19 10:45:26 -0300

    atualização do app

--- app1/app1/App.js
+++ app1/app1/App.js
@@ -1,23 +1,27 @@
 
-import React, { Component } from 'react';
+// import React, { Component } from 'react';
+import React from 'react';
 import { View, Text } from 'react-native';
+import C1 from './componentes/comp1'
 
-export default class App1 extends Component {
-  render() {
-    return (
-      <View>
-        <Text>Jadson Silva</Text>
-      </View>
-    );
-  }
-}
-
-
-// export default function App1(){
-//   return(
-//     <View>
-//       <Text>Jadson Silva</Text>
-//       <Text>Programador</Text>
-//     </View>
-//   )
+// export default class App1 extends Component {
+//   render() {
+//     return (
+//       <View>
+//         <Text>Jadson Silva</Text>
+//       </View>
+//     );
+//   }
 // }
+
+
+export default function App1() {
+  return (
+    <View>
+      <C1 />
+      <C1 />
+      <C1 />
+      <C1 />
+    </View>
+  );
+};
---
+++ app1/app1/componentes/comp1.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { View, Text } from 'react-native';
+
+
+export default function () {
+    return (
+        <Text>Jadson Silva - Programador React </Text>
+    )
+}
