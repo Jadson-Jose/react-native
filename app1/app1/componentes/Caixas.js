fcd1d31e9789e335221c74dc2495b17daf713273
JadsonJose <jadsonjosesilva@gmail.com>
2020-08-31 20:55:32 -0300

    Update Caixas.js

--- app1/app1/componentes/Caixas.js
+++ app1/app1/componentes/Caixas.js
@@ -3,10 +3,13 @@ import { View } from 'react-native';
 
 export default function (props) {
     return (
-        <View style={{ width: '100%', height: '100%', flexDirection: 'row', justifyContent: 'space-between', alignItems: 'center' }}>
-            <View style={{ width: 50, height: 50, backgroundColor: '#00f' }}></View>
-            <View style={{ width: 50, height: 50, backgroundColor: '#00a' }}></View>
-            <View style={{ width: 50, height: 50, backgroundColor: '#005' }}></View>
+        <View style={{ width: '100%', height: 300, flexDirection: 'column', justifyContent: 'center', aliginItems: 'stretch' }}>
+            <View style={{ flexGrow: 1, backgroundColor: '#00f' }}></View>
+            <View style={{ flexGrow: 1, backgroundColor: '#00a' }}></View>
+            <View style={{ flexGrow: 1, backgroundColor: '#005' }}></View>
+            <View style={{ flexGrow: 1, backgroundColor: '#f00' }}></View>
+            <View style={{ flexGrow: 1, backgroundColor: '#a00' }}></View>
+            <View style={{ flexGrow: 1, backgroundColor: '#500' }}></View>
 
         </View>
     )
