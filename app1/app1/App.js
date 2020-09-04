a041485f7bdeb607910c3159e779b46420bedc9c
JadsonJose <jadsonjosesilva@gmail.com>
2020-09-04 11:34:16 -0300

    sinc

--- app1/app1/App.js
+++ app1/app1/App.js
@@ -6,26 +6,25 @@ import Estilos from './estilos/Estilos';
 
 
 
-function fexibir(vp1) {
-  if (vp1) {
-    return (<Text>Curso de React Native</Text>)
-  } else {
-    return (<Text>Sem curso</Text>)
-  }
-}
+// function fexibir(vp1) {
+//   if (vp1) {
+//     return (<Text>Curso de React Native</Text>)
+//   } else {
+//     return (<Text>Sem curso</Text>)
+//   }
+// }
 
 
 
 export default function App1() {
-  let vexibir = false;
+  let vexibir = true;
   return (
     <View style={Estilos.container}>
-      <Caixas />
+      <Caixas exibir={vexibir} />
       <Text style={Estilos.textoPadrao}>Jadson jose da Silva</Text>
       <Text style={Estilos.textoTitulo}>Jadson jose da Silva</Text>
-      {fexibir(true)}
-
       {vexibir ? <Text>Curso React</Text> : <Text>Não curso</Text>}
+      {vexibir && <Text>Curso React2</Text>}
     </View>
   );
 };
