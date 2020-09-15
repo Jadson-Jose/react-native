667e2a4e41bc18796a6f5122b1ee482fc74d1838
JadsonJose <jadsonjosesilva@gmail.com>
2020-09-15 11:53:58 -0300

    sinc

--- app1/app1/App.js
+++ app1/app1/App.js
@@ -1,33 +1,39 @@
 import React from 'react';
-import { StyleSheet, View, Text, Image } from 'react-native';
+import {
+StyleSheet,
+View,
+Text,
+Image,
+Button,
+Alert
+} from 'react-native';
 // import C1 from '../app1/componentes/comp1';
 import Caixas from './componentes/Caixas';
 import Estilos from './estilos/Estilos';
 
 
-
-// function fexibir(vp1) {
-//   if (vp1) {
-//     return (<Text>Curso de React Native</Text>)
-//   } else {
-//     return (<Text>Sem curso</Text>)
-//   }
-// }
-
-
+function msg(){
+  Alert.alert('MSG', 'Curso de React Native')
+}
 
 export default function App1() {
   let vexibir = true;
   return (
 
     <View style={Estilos.container}>
+
+      <Button 
+        title="Mostrar mensagem"
+        onPress={msg}
+      />
+
       <Image
-        source={require('./assets/tema-red-bull.jpg')}
+        source={require('./assets/img/tema-red-bull2.jpg')}
         style={estilos.logo}
       />
 
 
-      <Caixas exibir={vexibir} />
+
       <Text style={Estilos.textoPadrao}>Jadson jose da Silva</Text>
       <Text style={Estilos.textoTitulo}>Jadson jose da Silva</Text>
       {vexibir ? <Text>Curso React</Text> : <Text>Não curso</Text>}
