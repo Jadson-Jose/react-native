1245d9bc9b9472080576f06e6fc76a577bcb5687
JadsonJose <jadsonjosesilva@gmail.com>
2020-10-26 18:03:03 -0300

    sinc

--- app1/app1/App.js
+++ app1/app1/App.js
@@ -24,6 +24,10 @@ function TelaHome({ navigation }) {
         title="Tela Canal"
         onPress={() => navigation.navigate('Canal')}
       />
+      <Button 
+      title = "Tela Cursos"
+      onPress = {()=>navigation.navigate('Cursos')}
+      />
     </View>
   );
 }
@@ -45,6 +49,41 @@ function TelaCanal({ navigation }) {
   );
 }
 
+function TelaCursos({ navigation }) {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Tela Cursos</Text>
+      <Button
+        title="React Native"
+        onPress={() => navigation.navigate('CursoReactNative', {
+          aulas: 100, autor: 'Jadson'
+        })}
+      />
+    </View>
+  );
+}
+function TelaCursoReactNative({ route, navigation }) {
+  // const { aulas } = route.params;
+  // const { autor } = route.params;
+  const aulas = route.params.aulas;
+  const autor = route.params.autor;
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>Curso de React Native</Text>
+      <Text>Aulas: {aulas}</Text>
+      <Text>Autor: {autor}</Text>
+      <Button
+        title="Home"
+        onPress={() => navigation.navigate('Home')}
+      />
+      <Button
+        title="Voltar para cursos"
+        onPress={() => navigation.goBack()}
+      />
+    </View>
+  );
+}
+
 export default function App1() {
   return (
     <NavigationContainer>
@@ -58,6 +97,16 @@ export default function App1() {
           name="Canal"
           component={TelaCanal}
           options={{ title: 'Tela Canal' }}
+        />
+        <Pilha.Screen
+          name="Cursos"
+          component={TelaCursos}
+          options={{ title: 'Cursos do Canal' }}
+        />
+        <Pilha.Screen
+          name="CursoReactNative"
+          component={TelaCursoReactNative}
+          options={{ title: 'Curso de React Native' }}
         />
       </Pilha.Navigator>
     </NavigationContainer>
